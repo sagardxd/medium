19b2d430c2df3ffb74b4280e21d8ce7214de5093
Sagar Yadav <sagardxd5@gmail.com>
2024-03-02 17:12:01 +0530

    signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,41 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
+import { getCookie, getSignedCookie, setCookie, setSignedCookie, deleteCookie } from 'hono/cookie'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string,
+    JWT_PASSWORD: string
+  }
+}>()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post('/api/v1/user/signup', async (c) => {
+  const body = await c.req.json();
 
-app.post('/api/v1/user/signup', (c) => {
-  return c.text("signup route")
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        name: body.name,
+        password: body.password
+      }
+    })
+
+    const jwt = await sign({id: user.id}, c.env.JWT_PASSWORD)
+    setCookie(c,"jwt",jwt)
+    c.status(201);
+    return c.text('signup hogya')
+
+  } catch (error) {
+    c.status(411);
+    return c.text("Invalid");
+  }
 })
 
 app.post('/api/v1/user/signin', (c) => {
@@ -31,6 +59,10 @@ app.get('/api/v1/blog/:id', (c) => {
 
 app.put('/api/v1/blog/bulk', (c) => {
   return c.text("blog bulk route")
+})
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
 })
 
 export default app
