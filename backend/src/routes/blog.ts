aad6cc3646457da1693ac1a72fecabb977b1f00d
Sagar Yadav <sagardxd5@gmail.com>
2024-03-03 16:28:15 +0530

    middleware

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,28 +1,51 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { getCookie} from 'hono/cookie'
+import { verify } from 'hono/jwt';
 
 export const blogRouter = new Hono<{
-    Bindings: {
-      DATABASE_URL: string,
-      JWT_PASSWORD: string
-    }
-  }>();
+  Bindings: {
+    DATABASE_URL: string,
+    JWT_PASSWORD: string
+  },
+  Variables: {
+    userId: string
+  }
+}>();
 
+//middleware
 blogRouter.use('/*', async (c, next) => {
+      const jwtCookie = getCookie(c, "jwt");
+      if(!jwtCookie) {
+        c.status(401);
+        return c.json({msg: "Not authorized"})
+      }
 
+      const payload = await verify(jwtCookie, c.env.JWT_PASSWORD);
+      if(!payload){
+        c.status(401);
+        return c.json({msg: "Not authorized"})
+      }
+      c.set("userId", payload.id)
+      await next();
 })
 
-blogRouter.post('/', async(c) => {
-    return c.text("blog route")
+blogRouter.post('/', async (c) => {
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  return c.text("blog route")
 })
 
-blogRouter.get('/:id', async(c) => {
-    const id = c.req.param('id');
-    console.log(id)
-    return c.text("blog id route")
+blogRouter.get('/:id', async (c) => {
+  const id = c.req.param('id');
+  console.log(id)
+  return c.text("blog id route")
 })
 
-blogRouter.put('/bulk', async(c) => {
-    return c.text("blog bulk route")
+blogRouter.put('/bulk', async (c) => {
+  return c.text("blog bulk route")
 })
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,70 +1,70 @@
-import {Hono} from 'hono'
-import {setCookie } from 'hono/cookie'
+import { Hono } from 'hono'
+import { setCookie } from 'hono/cookie'
 import { decode, sign, verify } from 'hono/jwt'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
 export const userRouter = new Hono<{
     Bindings: {
-      DATABASE_URL: string,
-      JWT_PASSWORD: string
+        DATABASE_URL: string,
+        JWT_PASSWORD: string
     }
-  }>();
+}>();
 
 userRouter.post('/signup', async (c) => {
     const body = await c.req.json();
-  
+
     const prisma = new PrismaClient({
-      datasourceUrl: c.env.DATABASE_URL,
+        datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate())
-  
+
     try {
-      const user = await prisma.user.create({
-        data: {
-          email: body.email,
-          name: body.name,
-          password: body.password
-        }
-      })
-  
-      const jwt = await sign({id: user.id}, c.env.JWT_PASSWORD)
-      setCookie(c,"jwt",jwt)
-      c.status(201);
-      return c.text('signup hogya')
-  
+        const user = await prisma.user.create({
+            data: {
+                email: body.email,
+                name: body.name,
+                password: body.password
+            }
+        })
+
+        const jwt = await sign({ id: user.id }, c.env.JWT_PASSWORD)
+        setCookie(c, "jwt", jwt)
+        c.status(201);
+        return c.text('signup hogya')
+
     } catch (error) {
-      c.status(411);
-      return c.text("Invalid");
+        c.status(411);
+        return c.text("Invalid");
     }
-  })
-  
-  userRouter.post('/signin', async(c) => {
+})
+
+userRouter.post('/signin', async (c) => {
     const body = await c.req.json();
-  
+
     const prisma = new PrismaClient({
-      datasourceUrl: c.env.DATABASE_URL,
+        datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate())
-  
+
     try {
-      const user = await prisma.user.findUnique({
-        where: {
-          email: body.email,
-          password: body.password
+        const user = await prisma.user.findUnique({
+            where: {
+                email: body.email,
+                password: body.password
+            }
+        })
+
+        if (!user) {
+            c.status(403);
+            return c.text("User does not exsist");
         }
-      })
-  
-      if(!user){
-        c.status(403);
-        return c.text("User does not exsist");
-      }
-  
-      const jwt = await sign({id: user.id}, c.env.JWT_PASSWORD)
-      setCookie(c,"jwt",jwt)
-      c.status(201);
-      return c.text('signin hogya')
-  
+
+        const jwt = await sign({ id: user.id }, c.env.JWT_PASSWORD)
+        setCookie(c, "jwt", jwt)
+        c.status(201);
+        return c.text('signin hogya')
+
     } catch (error) {
-      c.status(411);
-      return c.text("Invalid");
+        c.status(411);
+        return c.text("Invalid");
     }
-  })
+})
