a212d56410f9815cf85200963c2af5d64a2050d5
Sagar Yadav <sagardxd5@gmail.com>
2024-03-03 17:26:07 +0530

    blog create route

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { getCookie} from 'hono/cookie'
+import { getCookie } from 'hono/cookie'
 import { verify } from 'hono/jwt';
 
 export const blogRouter = new Hono<{
@@ -16,36 +16,59 @@ export const blogRouter = new Hono<{
 
 //middleware
 blogRouter.use('/*', async (c, next) => {
-      const jwtCookie = getCookie(c, "jwt");
-      if(!jwtCookie) {
-        c.status(401);
-        return c.json({msg: "Not authorized"})
-      }
+  const jwtCookie = getCookie(c, "jwt");
+  if (!jwtCookie) {
+    c.status(401);
+    return c.json({ msg: "Not authorized" })
+  }
+  try {
+    const payload = await verify(jwtCookie, c.env.JWT_PASSWORD);
+    if (!payload) {
+      c.status(401);
+      return c.json({ msg: "Not authorized" })
+    }
+    c.set("userId", payload.id)
+    await next();
+  } catch (error) {
+    console.log(error)
+    return c.json("error")
+  }
 
-      const payload = await verify(jwtCookie, c.env.JWT_PASSWORD);
-      if(!payload){
-        c.status(401);
-        return c.json({msg: "Not authorized"})
-      }
-      c.set("userId", payload.id)
-      await next();
 })
 
-blogRouter.post('/', async (c) => {
 
+
+blogRouter.post("/", async (c) => {
+  const body = await c.req.json();
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate())
 
-  return c.text("blog route")
+  try {
+    const userID = c.get('userId');
+    console.log('aa')
+    const blogPost = prisma.post.create({
+      data: {
+        title: body.title,
+        content: body.content,
+        authorId: userID
+      }
+    })
+    c.status(201);
+    return c.json("created")
+  } catch (error) {
+    c.status(411);
+    return c.json("error while creating post")
+  }
 })
 
-blogRouter.get('/:id', async (c) => {
+blogRouter.put('/:id', async (c) => {
   const id = c.req.param('id');
   console.log(id)
   return c.text("blog id route")
 })
 
-blogRouter.put('/bulk', async (c) => {
+blogRouter.get('/bulk', async (c) => {
+  console.log(c.get('userId'))
   return c.text("blog bulk route")
 })
