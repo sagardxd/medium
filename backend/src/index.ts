70122f58e8bc2c1777a36b94121b9ec8d733e785
Sagar Yadav <sagardxd5@gmail.com>
2024-03-04 11:01:29 +0530

    rm / route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,9 +14,4 @@ const app = new Hono<{
 app.route("/api/v1/user", userRouter)
 app.route("/api/v1/blog", blogRouter)
 
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
 export default app
