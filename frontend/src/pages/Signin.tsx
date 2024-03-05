7c405eab54bf3bb55fb17d9155d016c418e572e5
Sagar Yadav <sagardxd5@gmail.com>
2024-03-05 13:40:44 +0530

    auth route

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,15 +1,32 @@
 import { useState } from "react";
+import axios from "axios";
 import { SigninInput } from "@sagardxd/medium-common";
 import AuthHeader from "../components/AuthHeader"
 import Branding from "../components/Branding"
 import { LabelledInput } from "../components/LabelledInput";
+import { BACKEND_URL } from "../config";
+import { useNavigate } from "react-router-dom";
+
 
 const Signup = () => {
-
+    const navigate = useNavigate();
     const [postInputs, setpostInputs] = useState<SigninInput>({
         email: "",
         password: "",
     });
+
+    async function AuthRequest(){
+      console.log(postInputs)
+
+      try {
+         await axios.post(`${BACKEND_URL}/api/v1/user/signin`,postInputs);
+         navigate("/blog")
+      } catch (error) {
+        console.log(error)
+        alert("Error while signing up")
+      }
+    }
+
 
     return (
         <div className="grid grid-cols-1 lg:grid-cols-2">
@@ -26,10 +43,10 @@ const Signup = () => {
                         <LabelledInput label="Password" placeholder="123456" type={"password"} onChange={(e) => {
                             setpostInputs({
                                 ...postInputs,
-                                email: e.target.value
+                                password: e.target.value
                             })
                         }} />
-                        <button type="button" className="w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 
+                        <button type="button" onClick={AuthRequest} className="w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 
                 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700
                  dark:focus:ring-gray-700 dark:border-gray-700">Sign Up</button>
                     </div>
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -3,14 +3,29 @@ import { SignupInput } from "@sagardxd/medium-common";
 import AuthHeader from "../components/AuthHeader"
 import Branding from "../components/Branding"
 import { LabelledInput } from "../components/LabelledInput";
+import { BACKEND_URL } from "../config";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Signup = () => {
-
+    const navigate = useNavigate();
     const [postInputs, setpostInputs] = useState<SignupInput>({
         email: "",
         password: "",
         name: ""
     });
+
+    async function AuthRequest(){
+        console.log(postInputs)
+  
+        try {
+           await axios.post(`${BACKEND_URL}/api/v1/user/signup`,postInputs);
+           navigate("/blog")
+        } catch (error) {
+          console.log(error)
+          alert("Error while signing up")
+        }
+      }
 
     return (
         <div className="grid grid-cols-1 lg:grid-cols-2">
@@ -33,10 +48,10 @@ const Signup = () => {
                         <LabelledInput label="Password" placeholder="123456" type={"password"} onChange={(e) => {
                             setpostInputs({
                                 ...postInputs,
-                                email: e.target.value
+                                password: e.target.value
                             })
                         }} />
-                        <button type="button" className="w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 
+                        <button type="button" onClick={AuthRequest} className="w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 
                 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700
                  dark:focus:ring-gray-700 dark:border-gray-700">Sign Up</button>
                     </div>
