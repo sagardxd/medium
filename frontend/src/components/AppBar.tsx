dc3051694a5cccd62a4ca2deca780144d53eda9b
Sagar Yadav <sagardxd5@gmail.com>
2024-03-07 23:00:47 +0530

    appbar update

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -1,13 +1,16 @@
+import { Link } from "react-router-dom"
 import { Avatar } from "./BlogCard"
 
 const AppBar = () => {
   return (
     <div className="border-b flex justify-between px-10 p-4">
-      <div className="flex flex-col justify-center text-lg">
-      Medium
+      <Link to={"/blogs"}>
+      <div className="flex flex-col justify-center text-lg cursor-pointer font-medium">
+        Medium
       </div>
+      </Link>
       <div>
-      <Avatar name="Sagar" size="big"/>
+        <Avatar name="Sagar" size="big" />
       </div>
     </div>
   )
