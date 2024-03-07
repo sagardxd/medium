6bbc21d6464ec3424537929c81f46a006f08fa6c
Sagar Yadav <sagardxd5@gmail.com>
2024-03-07 23:10:10 +0530

    appbar new button

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -5,11 +5,14 @@ const AppBar = () => {
   return (
     <div className="border-b flex justify-between px-10 p-4">
       <Link to={"/blogs"}>
-      <div className="flex flex-col justify-center text-lg cursor-pointer font-medium">
-        Medium
-      </div>
+        <div className="flex flex-col justify-center text-lg cursor-pointer font-medium">
+          Medium
+        </div>
       </Link>
       <div>
+        <Link to={"/publish"}>
+          <button type="button" className="text-white mr-4 bg-green-500 hover:bg-green-800 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">New</button>
+        </Link>
         <Avatar name="Sagar" size="big" />
       </div>
     </div>
