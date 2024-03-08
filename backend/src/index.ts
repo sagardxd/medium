e87e2810c62358f9af9868f013c0c3b161e02835
Sagar Yadav <sagardxd5@gmail.com>
2024-03-08 09:12:26 +0530

    added origin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ const app = new Hono<{
 }>()
 
 app.use("/*", cors({
-  origin: 'https://medium-one-fawn.vercel.app/', // Adjust this to match your frontend origin
+  origin: 'https://medium-snorlxz.vercel.app', // Adjust this to match your frontend origin
   credentials: true,
  }));
 
