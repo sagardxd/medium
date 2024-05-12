dd781878dd88c62a389045905028f0e729593d6b
Sagar Yadav <sagardxd5@gmail.com>
2024-05-12 17:55:58 +0530

    landing-page

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
 import { Link } from "react-router-dom"
+import { BackgroundBeams } from '../components/ui/background-beams';
+import React from 'react';
 
 const LandingPage: React.FC = () => {
+
   return (
-    <div className="bg-gradient-to-br from-blue-500 to-purple-500 min-h-screen flex flex-col justify-center items-center">
-      <div className="max-w-lg mx-auto px-4 py-8 text-center text-white">
-        <h1 className="text-5xl font-bold mb-4">Welcome to Medium Clone</h1>
-        <p className="text-lg mb-8">Your go-to platform for reading and publishing articles.</p>
-        <div className="flex flex-col sm:flex-row gap-4">
-        <Link to={"/signup"}>
-          <button className="bg-white text-blue-500 hover:bg-blue-600 hover:text-white font-semibold py-3 px-6 rounded-full shadow-md transition duration-300">
-            Sign Up
-          </button>
-          </Link>
-        <Link to={"/signin"}>
-          <button className="bg-transparent text-white border border-white hover:bg-white hover:text-black font-semibold py-3 px-6 rounded-full shadow-md transition duration-300">
-            Sign In
-          </button>
-          </Link>
+    <div>
+      <div className="h-screen w-full  bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+        <div className="max-w-2xl mx-auto p-4">
+          <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+            <span className='text-white'>Medium</span> a way to know <span className='text-white'>World</span>
+          </h1>
+          <p></p>
+          <p className="text-white  max-w-lg mx-auto my-2 text-md text-center relative z-10">
+            Your go-to platform for reading and publishing articles..
+          </p>
+          <div className='flex justify-center gap-4 pt-3'>
+          <Link to={"/signin"}>
+            <button className="inline-flex h-12 relative z-10  animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+              Sign In
+            </button>
+            </Link>
+            <Link to={"/signup"}>
+            <button className="inline-flex  relative z-10  h-12 animate-shimmer items-center justify-center rounded-md border border-white-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+              Sign Up
+            </button>
+            </Link>
+
+          </div>
         </div>
+        <BackgroundBeams />
       </div>
-    </div>
+      </div>
   );
 };
 
--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -50,7 +50,7 @@ const Signup = () => {
                         }} />
                         <button type="button" onClick={AuthRequest} className="w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 
                 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700
-                 dark:focus:ring-gray-700 dark:border-gray-700">Sign Up</button>
+                 dark:focus:ring-gray-700 dark:border-gray-700">Sign In</button>
                     </div>
                 </div>
             </div>
